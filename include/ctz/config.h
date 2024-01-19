409a76d29ee6b83d782b43687c5607ea788ffd89
Ciel <createdbycrt@gmail.com>
2024-01-19 19:10:18 +0800

    init

---
+++ include/ctz/config.h
@@ -0,0 +1,43 @@
+#ifndef CTZ_CONFIG_H
+#define CTZ_CONFIG_H
+
+// debug level
+#if !defined(NDEBUG) || defined(DCHECK_ALWAYS_ON)
+#define CTZ_DEBUG_ENABLED 1
+#else
+#define CTZ_DEBUG_ENABLED 0
+#endif
+
+// namespace ctz
+#define NAMESPACE_CTZ_BEGIN namespace ctz {
+#define NAMESPACE_CTZ_END }
+
+NAMESPACE_CTZ_BEGIN
+
+void fatal(const char* msg, ...);
+void warn(const char* msg, ...);
+
+#if CTZ_DEBUG_ENABLED
+
+#define CTZ_FATAL(msg, ...) ctz::fatal(msg "\n", ##__VA_ARGS__);
+
+#define CTZ_ASSERT(cond, msg, ...)                   \
+do {                                                \
+    if (!(cond)) {                                  \
+        CTZ_FATAL("ASSERT: " msg, ##__VA_ARGS__);   \
+    }                                               \
+} while (false)
+
+#define CTZ_WARN(msg, ...) ctz::warn("WARNING: " msg "\n", ##__VA_ARGS__);
+
+#else
+
+#define CTZ_FATAL(msg, ...)
+#define CTZ_ASSERT(cond, msg, ...)
+#define CTZ_WARN(msg, ...)
+
+#endif
+
+NAMESPACE_CTZ_END
+
+#endif // CTZ_CONFIG_H
---
+++ src/config.cpp
@@ -0,0 +1,24 @@
+#include <ctz/config.h>
+
+#include <cstdarg>
+#include <cstdio>
+#include <cstdlib>
+
+NAMESPACE_CTZ_BEGIN
+
+void fatal(const char* msg, ...) {
+    va_list vararg;
+    va_start(vararg, msg);
+    vfprintf(stderr, msg, vararg);
+    va_end(vararg);
+    abort();
+}
+
+void warn(const char* msg, ...) {
+    va_list vararg;
+    va_start(vararg, msg);
+    vfprintf(stdout, msg, vararg);
+    va_end(vararg);
+}
+
+NAMESPACE_CTZ_END
